5de0dc0018274ab917ff5d06376c81c248dbf0ba
Darkecco <24685924+Darkecco@users.noreply.github.com>
2021-08-10 09:51:47 +0200

    Create changeDate-fullmonths.js

---
+++ changeDate-fullmonths.js
@@ -0,0 +1,56 @@
+let myInput = DoButton.doButtonNewCommandCommon.OccurredAt; // Change the DoButton Trigger to the one used in the IFTTT recipe.
+let myDateTime = myInput.split("at").map(function(item) {
+  return item.trim();
+});
+
+function Pad(str: string) { // This function pads single digit numbers with a preceeding 0.
+  if (str.length < 2) {
+    return "0" + str;
+  } else {
+    return str;
+  }
+}
+
+let myMonths = {
+  "jan": "Januari",
+  "feb": "Februari",
+  "mar": "Maart",
+  "apr": "April",
+  "may": "Mei",
+  "jun": "Juni",
+  "jul": "Juli",
+  "aug": "Augustus",
+  "sep": "September",
+  "oct": "Oktober",
+  "nov": "November",
+  "dec": "December"
+};
+
+let myDate = myDateTime[0].split(" ");
+myDate[0] = myMonths[myDate[0].substr(0, 3).toLowerCase()];
+myDate[1] = myDate[1].replace(",", "");
+myDate[1] = Pad(myDate[1]);
+let myDateCorrected = [myDate[1], myDate[0], myDate[2]];
+myDateTime[0] = myDateCorrected.join(" "); // You can change this delimiter to what you need in the date ouput, i.e. "-" or "/".
+
+let my12Hour = myDateTime[1].substr(-2, 2);
+let myHour = parseInt(myDateTime[1].substr(0, 2));
+let myMinute = myDateTime[1].substr(3, 2);
+
+if (myHour != 12) {
+  if (my12Hour == "PM") {
+    myHour = (myHour + 12) % 24;
+  }
+} else {
+  if (my12Hour == "AM") {
+    myHour = 0;
+  }
+}
+
+let myHourStr = myHour.toString();
+myHourStr = Pad(myHourStr);
+
+myDateTime[1] = myHourStr + ":" + myMinute;
+let myOutput = myDateTime.join(" ")
+
+IfNotifications.sendNotification.setMessage("Button pressed @ " + myOutput)
